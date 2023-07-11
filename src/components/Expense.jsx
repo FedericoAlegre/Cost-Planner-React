105e82d629010677a201b4bb3d9ade8b42dab428
FedericoAlegre <fedealegre.mdp@gmail.com>
2023-07-11 18:54:38 -0300

    icons

--- src/components/Expense.jsx
+++ src/components/Expense.jsx
@@ -2,7 +2,21 @@ import { useState } from "react"
 import {LeadingActions, SwipeableList, SwipeableListItem, SwipeAction, TrailingActions} from 'react-swipeable-list'
 import "react-swipeable-list/dist/styles.css"
 import { formatDate } from "../helpers"
+import FoodIcon from "../img/icono_food.svg"
+import HouseIcon from "../img/icono_house.svg"
+import SavingsIcon from "../img/icono_savings.svg"
+import PleasureIcon from "../img/icono_pleasure.svg"
+import HealthIcon from "../img/icono_health.svg"
+import SubsIcon from "../img/icono_subscription.svg"
 
+const Icons = {
+    savings: SavingsIcon,
+    food: FoodIcon,
+    house: HouseIcon,
+    health: HealthIcon,
+    subscription: SubsIcon,
+    pleasure: PleasureIcon
+}
 
 const Expense = ({expense, setEditExpense, deleteExpense}) => {
 
@@ -31,7 +45,7 @@ const Expense = ({expense, setEditExpense, deleteExpense}) => {
             <div className="gasto sombra">
                 
             <div className="contenido-gasto">
-                <img src={`src/img/icono_${expense.category}.svg`}
+                <img src={Icons[expense.category]}
                 alt="icon-expense"></img>
                 <div className="descripcion-gasto">
                     <p className="categoria">{expense.category}</p>
